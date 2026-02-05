06ac61b5c90438134370bbdf6eda5e32601e2a6a
Deniss Patancevs <deniss.patancevs@gmail.com>
2026-02-05 09:13:28 +0200

    refactor: render tech stack, courses and projects dynamically from JS data chore:    add Docker and Laravel to tech stack

---
+++ data/courses.js
@@ -0,0 +1,27 @@
+export const courses = [
+  {
+    title: "JS / Front-end Course (RS School)",
+    description: "26-week program followed by a 7-week final team project",
+    repo: "https://github.com/deniss87/JSFEEN2024Q4",
+    website: "https://rs.school/courses/javascript",
+    provider: "RS School",
+    tags: ["JavaScript", "Frontend", "Team Project"],
+  },
+  {
+    title: "React Course (RS School)",
+    description: "8-week program with hands-on projects",
+    repo: "https://github.com/deniss87/REACT2025Q3",
+    website: "https://rs.school/courses/reactjs",
+    provider: "RS School",
+    tags: ["React", "TypeScript", "SPA"],
+  },
+  {
+    title: "Web Applications (Coursera)",
+    description: "PHP, MySQL, JavaScript specialization",
+    repo: "https://github.com/deniss87/course-web-applications-for-everybody",
+    website:
+      "https://www.coursera.org/specializations/web-applications",
+    provider: "Coursera",
+    tags: ["PHP", "MySQL", "Backend"],
+  },
+];
